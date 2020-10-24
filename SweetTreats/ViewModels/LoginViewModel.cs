f925b8e2492869feb992310cb2137bfcb89cbce7
Michael Watts <wattsjmichael@gmail.com>
2020-10-24 08:29:20 -0700

    Added AddFlavor to the Treat View

--- SweetTreats/ViewModels/LoginViewModel.cs
+++ SweetTreats/ViewModels/LoginViewModel.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace SweetTreats.ViewModels
 {
     public class LoginViewModel
-    {
+    { 
+        [Required]  
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
